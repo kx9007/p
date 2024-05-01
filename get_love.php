209afdf9e4d9c6d441ef1edc2034dc923a38722d
kx9007 <159279975+kx9007@users.noreply.github.com>
2024-05-01 19:38:30 +0800

    Delete get_love.php

--- get_love.php
+++
@@ -1,8 +0,0 @@
-<?php
-    if (isset($_POST['pass']) && $_POST['pass'] == '9007') {
-        $fileContent = file_get_contents("config/love.txt");
-        echo $fileContent;
-    } else {
-        echo "获取失败";
-    }
-?>
